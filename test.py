34e4b6272c8ef3b0989de27c98f05cab530d0c41
mwalaa <mariam.walaa16@gmail.com>
2020-09-03 00:03:38 -0400

    added more tests

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from simpsons_paradox import SimpsonsParadox
@@ -14,10 +15,8 @@ def test_fcn(**test_params):
         number of Simpson's pairs found in data
 
     """
-    return len(
-        SimpsonsParadox(
-            **test_params.update({'quiet': True})
-            ).get_simpsons_pairs())
+    test_params.update({'quiet': True})
+    return len(SimpsonsParadox(**test_params).get_simpsons_pairs())
 
 
 if __name__ == '__main__':
@@ -88,4 +87,46 @@ if __name__ == '__main__':
     }
     BASEBALL_RESULTS = test_fcn(**baseball_params)
     assert BASEBALL_RESULTS == 1
+    print('Passed!')
+
+    # Test 7: Coffee Data
+    print('Testing the Coffee data...')
+    coffee_df = pd.read_csv('data/coffee_data.csv')
+    coffee_params = {
+        'df': coffee_df, 'dv': 'neuroticism', 'ignore_columns': ['Unnamed: 0']}
+    COFFEE_RESULTS = test_fcn(**coffee_params)
+    assert COFFEE_RESULTS == 0
+    print('Passed!')
+
+    # Test 8: Student Performance Data
+    print('Testing the Student Performance data...')
+    student_df = pd.read_csv('data/student-mat.csv', sep=';')
+    student_df['final_grade'] = np.where(student_df['G3'] >= 10, 1, 0)
+    student_params = {'df': student_df, 'dv': 'final_grade'}
+    STUDENT_RESULTS = test_fcn(**student_params)
+    assert STUDENT_RESULTS == 2
+    print('Passed!')
+
+    # Test 9: Teaching Assistant Data
+    print('Testing the Teaching Assistant Evaluation data....')
+    teaching_df = pd.read_csv('data/TeachingAssistant_EvaluationDataset.csv')
+    teaching_params = {'df': teaching_df, 'dv': 'Score', 'target_category': 1}
+    TEACHING_RESULTS = test_fcn(**teaching_params)
+    assert TEACHING_RESULTS == 0
+    print('Passed!')
+
+    # Test 10: Breast Cancer Data
+    print('Testing the Breast Cancer data...')
+    cancer_df = pd.read_csv('data/breastcancer_data.csv')
+    cancer_params = {'df': cancer_df, 'dv': 'class'}
+    CANCER_RESULTS = test_fcn(**cancer_params)
+    assert CANCER_RESULTS == 0
+    print('Passed!')
+
+    # Test 10: Breast Cancer Data
+    print('Testing the Breast Cancer data...')
+    cancer_df = pd.read_csv('data/breastcancer_data.csv')
+    cancer_params = {'df': cancer_df, 'dv': 'class', 'max_pvalue': 1}
+    CANCER_RESULTS = test_fcn(**cancer_params)
+    assert CANCER_RESULTS == 2
     print('Passed!')
